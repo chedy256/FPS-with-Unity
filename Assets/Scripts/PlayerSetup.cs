d4ae6a7278f214e12ddb55f1d9a257ee2926ecf4
chedy256 <chedyelhaj@gmail.com>
2021-05-16 13:46:33 +0100

    1

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using Mirror;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(PlayerController))]
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    private string remoteLayerName = "RemotePlayer";
+
+    [SerializeField]
+    private string dontDrawLayerName = "DontDraw";
+
+    [SerializeField]
+    private GameObject playerGraphics;
+
+    [SerializeField]
+    private GameObject playerUIPrefab;
+
+    [HideInInspector]
+    public GameObject playerUIInstance;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            // Désactiver la partie graphique du joueur local
+            Util.SetLayerRecursively(playerGraphics, LayerMask.NameToLayer(dontDrawLayerName));
+
+            // Création du UI du joueur local
+            playerUIInstance = Instantiate(playerUIPrefab);
+
+            // Configuration du UI
+            PlayerUI ui = playerUIInstance.GetComponent<PlayerUI>();
+            if(ui == null)
+            {
+                Debug.LogError("Pas de component PlayerUI sur playerUIInstance");
+            }
+            else
+            {
+                ui.SetPlayer(GetComponent<Player>());
+            }
+
+            GetComponent<Player>().Setup();
+
+            CmdSetUsername(transform.name, UserAccountManager.LoggedInUsername);
+        }
+    }
+
+    [Command]
+    void CmdSetUsername(string playerID, string username)
+    {
+        Player player = GameManager.GetPlayer(playerID);
+        if(player != null)
+        {
+            Debug.Log(username+ " has joined !");
+            player.username = username;
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string netId = GetComponent<NetworkIdentity>().netId.ToString();
+        Player player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(netId, player);
+    }
+
+    private void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    private void DisableComponents()
+    {
+        // On va boucler sur les différents composants renseignés et les désactiver si ce joueur n'est pas le notre
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Destroy(playerUIInstance);
+
+        if(isLocalPlayer)
+        {
+            GameManager.instance.SetSceneCameraActive(true);
+        }
+
+        GameManager.UnregisterPlayer(transform.name);
+    }
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform thrusterFuellFill;
+
+    [SerializeField]
+    private RectTransform HealthBarFill;
+    
+    private Player player;
+    private PlayerController controller;
+    private WeaponManager weaponManager;
+
+    [SerializeField]
+    private Text ammoText;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    [SerializeField]
+    private GameObject scoreboard;
+
+    public void SetPlayer(Player _player)
+    {
+        player = _player;
+        controller = player.GetComponent<PlayerController>();
+        weaponManager = player.GetComponent<WeaponManager>();
+    }
+
+    private void Start()
+    {
+        PauseMenu.isOn = false;
+    }
+
+    private void Update()
+    {
+        SetFuelAmount(controller.GetThrusterFuelAmount());
+        SetHealthAmount(player.GetHealthPct());
+        SetAmmoAmount(weaponManager.currentMagazineSize);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            scoreboard.SetActive(true);
+        }else if(Input.GetKeyUp(KeyCode.Tab))
+        {
+            scoreboard.SetActive(false);
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.isOn = pauseMenu.activeSelf;
+    }
+
+    void SetFuelAmount(float _amount)
+    {
+        thrusterFuellFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+    
+    void SetHealthAmount(float _amount)
+    {
+        HealthBarFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+
+     void SetAmmoAmount(int _amount)
+    {
+        //Remplir le text avec le numbre de munitions
+        ammoText.text = _amount.ToString();
+    }
+}
